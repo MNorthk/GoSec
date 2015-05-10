1b800e843b42387c6459b56787aef7f4b2337e30
潘铭 <panda_m@126.com>
2015-05-11 00:49:32 +0800

    修改
    修改了发布信息页面的一些代码

--- UI/ExpressTest2/js/publish.js
+++ UI/ExpressTest2/js/publish.js
@@ -4,7 +4,7 @@ function bfPub() {
     var gInfo = $("#gInfo").val();
     var h = $("#time-h").val();
     var m = $("#time-m").val();
-
+   
     //开始验证信息
 
     if (Number(h) > 23 || Number(h) < 0 || Number(m) > 60 || Number(m) < 0) {
@@ -29,14 +29,50 @@ function bfPub() {
         takeGoodsAdress: tAddr,
         goodsInfo: gInfo,
         takeTime: ttime
+        
     }
+   
     publish(obj);
 
 }
 
 function publish(obj) {
+    alert(obj);
     $.post("/Goods/setGoodsInfoIn", obj, function (d) {
         //回调
-        alert(d);
+        alert(gInfo);
+        switch(d)
+        {
+            case '200':
+                {
+                    alert("发布成功~");
+                    Location.href = 'index.html';
+                    break;
+                }
+            case '204':
+                {
+                    aler('时间写的不对吧~');
+                    break;
+                }
+            case "201":
+                {
+                    alert("信息不完整~");
+                    break;
+                }
+            case '203':
+                {
+                    aler("登录超时，请重新登录~");
+                    break;
+                }
+            case '202':
+                {
+                    alert("信息发布失败，请重试~");
+                    break;
+                }
+            default:
+                alert("哎呀，不知道哪里出错了~");
+                break;
+        }
+         
     });
 }
